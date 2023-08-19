4548f3cc9cc2dd737374d1a6ce51cd24f89be06f
DalatuPeter <Pitkaguru5@gmail.com>
2023-08-19 16:41:29 -0700

    Complete

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -6,18 +6,19 @@
     of the program.
 */
 
-
 if (process.argv.length <= 2) {
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
     process.stderr.write("Example: ./1-print_square.js 8\n");
-    process.exit(1)
+    process.exit(1);
 }
 
-size = parseInt(process.argv[2], 10)
+// Convert the argument to an integer using base 10, not base 16.
+// Using base 16 would interpret the input as a hexadecimal number.
+const size = parseInt(process.argv[2], 10);
 
-for (let i = 0 ; i < size ; i ++) {
-    for (let j = 0 ; j < size ; j ++) {
+for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
         process.stdout.write("#");
     }
     process.stdout.write("\n");
